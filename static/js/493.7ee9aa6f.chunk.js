02586c28aa8c809450836e9bd4069bd785a499ce
Stylize-K <121068672+Stylize-K@users.noreply.github.com>
2023-07-28 23:29:43 +0000

    Deploying to gh-pages from @ Stylize-K/goit-react-hw-05-movies@fc2a8a77f27580396a1d9974cd4dd48f2ad58e8e 🚀

---
+++ static/js/493.7ee9aa6f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[493],{493:function(e,t,i){i.r(t);var c=i(184);t.default=function(){return(0,c.jsx)("div",{children:"Home page"})}}}]);
+//# sourceMappingURL=493.7ee9aa6f.chunk.js.map
