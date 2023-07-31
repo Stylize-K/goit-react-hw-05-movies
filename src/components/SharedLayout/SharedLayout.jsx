aa1a2e09d829bab3d7977348fd2ac2222126049d
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-08-01 01:38:02 +0300

    Add SharedLayout

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,12 +1,18 @@
 import { Suspense } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
+import css from './SharedLayout.module.css';
+
 export const SharedLayout = () => {
   return (
     <div>
       <header>
         <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/movies">Movies</NavLink>
+          <NavLink className={css.navlink} to="/">
+            Home
+          </NavLink>
+          <NavLink className={css.navlink} to="/movies">
+            Movies
+          </NavLink>
         </nav>
       </header>
       <main>
