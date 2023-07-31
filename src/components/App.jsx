773f3c2742d7c364aa9af27235472e565e080ad8
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-08-01 01:36:03 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 // import Home from './pages/Home';
 // import Movies from './pages/Movies';
 // import MovieDetails from './pages/MovieDetails';
-import { SharedLayout } from './SharedLayout ';
-import { Cast } from './Cast';
-import { Reviews } from './Reviews';
+import { SharedLayout } from './SharedLayout/SharedLayout';
+import { Cast } from './Cast/Cast';
+import { Reviews } from './Reviews/Reviews';
 
-const Home = lazy(() => import('./pages/Home'));
-const Movies = lazy(() => import('./pages/Movies'));
-const MovieDetails = lazy(() => import('./pages/MovieDetails'));
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
 export const App = () => {
   return (
     <Routes>
