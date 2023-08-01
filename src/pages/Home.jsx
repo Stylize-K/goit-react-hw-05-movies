144044e7bd7ddd84e0241d3980699e91ec1e8069
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-08-02 00:17:16 +0300

    Add spinner for async

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,22 @@
 import { useEffect, useState } from 'react';
 import { fetchTrends } from '../services/apiService';
 import { MoviesList } from '../components/MoviesList/MoviesList';
+import { ColorRing } from 'react-loader-spinner';
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setIsLoading(true);
         const data = await fetchTrends();
         setMovies([...data.results]);
       } catch (error) {
         console.log('Error', error.message);
       } finally {
-        // setIsLoading(false);
+        setIsLoading(false);
       }
     };
     fetchData();
@@ -23,6 +26,17 @@ const Home = () => {
     <>
       <h1>Tranding Today</h1>
       <MoviesList movies={movies} />
+      {isLoading && (
+        <ColorRing
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="blocks-loading"
+          wrapperStyle={{}}
+          wrapperClass="blocks-wrapper"
+          colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+        />
+      )}
     </>
   );
 };
